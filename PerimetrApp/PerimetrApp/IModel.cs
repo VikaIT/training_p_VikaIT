c0ff5db183cbc96961cc1d2e12aa66e741270a54
VikaIT <75212958+VikaIT@users.noreply.github.com>
2021-02-11 20:45:07 +0300

    Create IModel.cs

---
+++ PerimetrApp/PerimetrApp/IModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetrApp
+{
+	/// <summary>
+	/// Протокол для разработки подсчета периметра прямоугольника
+	/// </summary>
+	interface IModel
+	{
+		/// <summary>
+		/// Методб, возвращающий периметр прямоугольника по двум сторанам
+		/// </summary>
+		/// <param name="a">Одна сторна прямоугольника</param>
+		/// <param name="b">Другая сторна прямоугольника</param>
+		/// <returns></returns>
+		int GetP(int a, int b);
+	}
+}
